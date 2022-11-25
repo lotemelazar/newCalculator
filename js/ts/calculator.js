a431e2770de6477170a0a861f8fdb524618343f9
lotemelazar <elazarlotem1808@gmail.com>
2022-11-25 12:45:48 +0200

    initial commit

---
+++ js/ts/calculator.js
@@ -0,0 +1,4 @@
+let curr_val = 0;
+let first_num = null;
+let curr_op = null;
+let second_num = null;
---
+++ ts/app.ts
@@ -0,0 +1,81 @@
+function changetext(val) {
+    document.getElementById('screen').textContent += val.innerHTML;
+}
+function get_info() {
+    alert("developer name: Lotem Elzaer \n version: II \n description: a calculator");
+}
+// function displayButtonInfo(val) {
+//     alert(val);
+// }
+
+
+// function changemode() {
+//     var element = document.body;
+//     element.classList.toggle("dark_mode");
+
+// }
+
+
+function sciMod() {
+    var e = document.body;
+    e.classList.toggle("scimod");
+
+
+
+
+}
+
+function Reset(): void {
+    document.getElementById('screen').textContent = '';
+}
+
+function oneBack(): void {
+    document.getElementById('screen').innerHTML = document.getElementById('screen').innerHTML.slice(0, -1);
+}
+
+
+function calculate(): void {
+    let ans = eval(document.getElementById('screen').textContent);
+    document.getElementById('screen').textContent = ans
+
+}
+
+
+function configSettings() {
+    const data = window.location.search;
+    let params = new URLSearchParams(data);
+    const colors = params.get("colors");
+    const fonts = params.get("fonts");
+    const mode = params.get("mode");
+    if (colors) {
+        document.body.style.background = colors;
+    }
+    if (fonts) {
+        document.body.style.fontFamily = fonts;
+    }
+    if (mode === "dark") {
+
+        let element = document.body;
+        element.classList.toggle("dark_mode");
+    }
+
+}
+
+
+
+document.querySelectorAll('.op').forEach(item => {
+    item.addEventListener('click', event => {
+        changetext(item)
+    })
+})
+
+const nums = (document.querySelectorAll('.numbers'));
+for (let i = 0; i < nums.length; i++) {
+    nums[i].addEventListener('click', function () { changetext(nums[i]) });
+}
+
+document.getElementById('equal').addEventListener('click', () => calculate());
+document.getElementById('settings').addEventListener('click', () => configSettings());
+document.getElementById('back').addEventListener('click', () => oneBack());
+document.getElementById('C').addEventListener('click', () => Reset());
+document.getElementById('sci').addEventListener('click', () => sciMod())
